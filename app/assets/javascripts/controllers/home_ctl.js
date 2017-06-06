6c9bc072538c2d31612b3cba2cc9c0a6027542b3
EnalMulla <enal4.mu@gmail.com>
2017-06-06 15:55:50 +0300

    commit02

--- app/assets/javascripts/controllers/home_ctl.js
+++ app/assets/javascripts/controllers/home_ctl.js
@@ -1,8 +1,8 @@
 /*globals angular , window, unused, _  */
-angular.module('app.controllers').controller('homeCtl', function ($scope) {
+angular.module('app.controllers').controller('homeCtl', ['$scope', function ($scope) {
   'use strict';
   console.log('angular is up');
   $scope.init = function () {
     $scope.ng = 'angular is up';
   };
-});
+}]);
